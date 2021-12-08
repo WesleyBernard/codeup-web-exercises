2974153b65c6b24d818722c559dc9e80cae38553
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-08 12:59:36 -0500

    Adding hello-asteroid.js

---
+++ js/hello-asteroid.js
@@ -0,0 +1,5 @@
+var A = "I got tired of writing out my favorite food so I made a couple of variables. ";
+
+var B = "My favorite food is hotpot. (I guess that's more of a type than a specific food...)";
+
+console.log(A + B);
