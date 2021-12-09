352545cd0000390cffef85bcab5830669f3a9a9a
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-09 10:13:54 -0500

    Adding comments and maybe a line of code, I dunno I was on the struggle bus

--- js/operators-exercise.js
+++ js/operators-exercise.js
@@ -1,3 +1,6 @@
 var sample = "Hello Codeup"
 console.log(sample);
     // I'm smooth brained and didn't finish this, but I will I swear
+console.log(sample.indexOf());
+
+//now I'm having trouple with c logging oh gosh oh heck
