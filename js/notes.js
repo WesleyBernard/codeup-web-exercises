c67863bc13d181679ee91e8df24f3e49c94c2d4e
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-09 12:18:10 -0500

    adding a notes file

---
+++ js/notes.js
@@ -0,0 +1,31 @@
+//--NUMBERS (INTRO)
+//I hope it's cool for me to take my notes in here
+//anyway, expressions in js will follow order of operations just like we did in middle school, (PEMDAS)
+
+//console.log(Math.PI);
+
+// let exp = (10 * 2 / 5) - 2**2
+//
+// console.log(exp);
+//
+// // This will bring back
+// //
+// // 10 x 2
+// // then 20 / 4
+// // then 2 ** 2, 2 to the second power
+// // then 4 - 4
+
+// Fun fact 1/0 is infinity and 0/1 is 0 (I think)
+
+// -Binary operator require two operands in order to evaluate
+// add, subtract, blah blah, here are the ones you might forget, but willneed
+// % modulus
+// **exponent
+// That's it for now
+
+// We also have unary operators
+// + - plus
+// - - negate
+// ++ - increment
+// -- - decrement
+
