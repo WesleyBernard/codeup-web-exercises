6c6a0b33d327afef40579c4673aa38942b6c2472
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-09 13:03:47 -0500

    Adding data types, ops, and var exercises and updating operators exercies

---
+++ js/datatypes-operators-variables.js
@@ -0,0 +1,57 @@
+// 1
+var a = 1;
+var b = a++;
+var c = ++a;
+// what is the value of a, b, and c?
+console.log(a);
+
+console.log(b);
+
+console.log(c);
+
+var d = "hello";
+var e = false;
+
+console.log(d++);
+console.log(e++);
+
+var perplexed; // perplexed is undefined (no value is assigned)
+console.log(perplexed + 2);
+
+var price = 2.7;
+console.log(price.toFixed(2));
+
+//2
+var sample = "Hello Codeup"
+
+console.log(sample.length);
+
+console.log(sample.toUpperCase());
+
+var sample = sample + " Students"
+
+console.log(sample);
+
+sample = sample.replace("Students", "Class");
+
+console.log(sample);
+
+console.log(sample.indexOf("c")) //retuens -1
+
+console.log(sample.indexOf("C"));
+
+console.log(sample.indexOf("Codeup"));
+
+console.log(sample.substr(6, 6));
+
+//3
+
+var mermaid = 3
+var bear = 5
+var herc = 1
+
+console.log((3 + 5 + 1) * 3);
+
+
+
+
--- js/operators-exercise.js
+++ js/operators-exercise.js
@@ -1,6 +1,38 @@
-var sample = "Hello Codeup"
-console.log(sample);
-    // I'm smooth brained and didn't finish this, but I will I swear
-console.log(sample.indexOf());
+console.log('1' + 2);
 
-//now I'm having trouple with c logging oh gosh oh heck
+console.log(typeof '1' + 2);
+
+console.log(6 % 4);
+
+console.log('3 + 4 is ' + parseInt(3 + 4));
+// how could you make this produce the correct output?
+
+console.log(0 < 0);
+
+console.log('false' == false);
+
+console.log(true == 'true');
+
+console.log(5 >= -5);
+
+console.log(!false || false);
+
+console.log(true || "42");
+
+console.log(!true && !false);
+
+console.log(6 % 5);
+
+console.log(5 < 4 && 1 === 1);
+
+console.log(5 < 4 && 1 === 1);
+
+console.log(typeof 'codeup' === 'string');
+
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+
+console.log(4 >= 0 && 1 !== '1');
+
+console.log(6 % 3 === 0);
+
+console.log(5 % 2 !== 0);
