716a31bc6c653e2b087c2c6f8d13b182903ad41e
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-09 09:28:45 -0500

    modified asteroid and adding data types assignments

---
+++ js/Data-types.js
@@ -0,0 +1,13 @@
+99.9 - number
+
+"false" - string
+
+false -  boolean
+
+'0' - string
+
+0 - number
+
+true - boolean
+
+'true' - string
--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,5 +1,5 @@
-var A = "I got tired of writing out my favorite food so I made a couple of variables. ";
+var A = "I got tired of writing out my favorite food \n\tso I made a couple of variables. \n";
 
-var B = "My favorite food is hotpot. (I guess that's more of a type than a specific food...)";
+var B = "My favorite food is hotpot. \n(I guess that's more of a type than a specific food...)\n\t OwO";
 
 console.log(A + B);
