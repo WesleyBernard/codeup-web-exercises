d3f2db2d6982c00ca6694a58fab35b9a1fad5882
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-13 12:34:06 -0500

    Updating warmup js

--- js/warmup.js
+++ js/warmup.js
@@ -1,5 +1,14 @@
-var creditScore = 590
-var cash = 9000
-var accepted = (creditScore > 680 && cash >= 4000) || cash >= 10000;
+// var creditScore = 590
+// var cash = 9000
+// var accepted = (creditScore > 680 && cash >= 4000) || cash >= 10000;
+//
+// console.log(accepted);
 
-console.log(accepted);
+//Declare a function called 'isFive'
+// that takes in an input and returns true if that input is the number five. Otherwise it should return false.
+
+function isFive(x) {
+    return x === 5
+}
+
+console.log(isFive("5"));
