4afea50a0a602df5d0b83df0d22663407af80a9d
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-13 14:41:12 -0500

    Adding conditionals js/html and adding extra functions

---
+++ js/conditionals.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// function analyzeColor(color) {
+//     if (color === "red") {
+//         console.log("red is a firey color");
+//     }else if (color === "blue") {
+//         console.log("blue is a sad color");
+//     } else {
+//         console.log("I dunno much about that color");
+//     }
+// }
+//
+//
+// console.log(analyzeColor("green"));
+//
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// var randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+// console.log(randomColor);
+//
+// console.log(analyzeColor(randomColor));
+
+// let color = prompt("What is your favorite color?");
+//
+// switch(color) {
+//     case "red":
+//         alert("red is a firey color");
+//         break;
+//     case "blue":
+//      alert("blue is a sad color");
+//         break;
+//     default:
+//         alert("I dunno much about that color");
+// }
+//
+
+// function calculateTotal(x,y) {
+//     if (x === 0) {
+//         return y;
+//     } else if (x === 1) {
+//         return  y - .10 * y;
+//     } else if (x === 2) {
+//         return y - .25 * y;
+//     } else if (x === 3) {
+//         return y - .35 * y;
+//     } else if (x === 4) {
+//         return y - .50 * y;
+//     } else if (x === 5) {
+//         return 0;
+//     } else if (x === 0) {
+//         return y
+//     }
+// }
+//
+// console.log(calculateTotal(3,100));
+
+// var luckyNumber = Math.floor(Math.random() * 6);
+//
+// let y = parseInt(prompt("What was your total?"));
+//
+// function calculateTotal(luckyNumber,y) {
+//     if (luckyNumber === 0) {
+//         return y;
+//     } else if (luckyNumber === 1) {
+//         return  y - .10 * y;
+//     } else if (luckyNumber === 2) {
+//         return y - .25 * y;
+//     } else if (luckyNumber === 3) {
+//         return y - .35 * y;
+//     } else if (luckyNumber === 4) {
+//         return y - .50 * y;
+//     } else if (luckyNumber === 5) {
+//         return 0;
+//     } else if (luckyNumber === 0) {
+//         return y
+//     }
+// }
+//
+// console.log(calculateTotal(luckyNumber, y));
+//
+// alert("Your lucky number was " + luckyNumber + ", Your total was " + y + ", your amount to pay is " + (calculateTotal(luckyNumber, y)));
+//
+// alert("UwU");
+
+let want = confirm("Would you like to enter a number?");
