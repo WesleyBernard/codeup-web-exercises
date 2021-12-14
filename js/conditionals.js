dd37e1cb0a120e8176617ac096d4d8e6b7ceafe7
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-14 16:24:03 -0500

    adding conditionals project.html and updating conditions.js to include the project

--- js/conditionals.js
+++ js/conditionals.js
@@ -116,14 +116,85 @@
 // “I don’t know that color” if the value of color is anything else
 // HINT: use return in the conditional code blocks to prevent from printing multiple times for the same argument
 
-function analyzeColor1(color) {
-    if (color === "red") {
-    return "red is the color of love";
-    } if (color === "blue") {
-        return "blue is the color of the sky";
-    } if (color === "green") {
-        return "green is the color of envy";
-    } return "I don't know much about that color";
+// function analyzeColor1(color) {
+//     if (color === "red") {
+//         return "red is the color of love";
+//     } if (color === "blue") {
+//         return "blue is the color of the sky";
+//     } if (color === "green") {
+//         return "green is the color of envy";
+//     } return "I don't know much about that color";
+// }
+//
+// console.log(analyzeColor1("red"));
+// console.log(analyzeColor1("blue"));
+// console.log(analyzeColor1("green"));
+// console.log(analyzeColor1("yellow"));
+
+// function analyzeColor1(color) {
+//     if (color === "blue") {
+//         return "blue is the color of the sky";
+//     } if (color === "red") {
+//         return "red is the color of love";
+//     } if (color === "green") {
+//         return "green is the color of envy"}
+// }
+//
+// console.log(analyzeColor1("red"));
+// console.log(analyzeColor1("blue"));
+// console.log(analyzeColor1("green"));
+
+// here is a conditionals project for those who are done with the exercises:
+//     Create a file named analyze_input.js in the js directory.
+//     In that file, write a program that receives user input and analyzes the input differently
+//     if the input is a number or a string. If the input is a string, it will analyze the string differently
+//     if it is length 1.
+// Following are the rules of analysis:
+//     If the input is a number:
+//     Output if the number is odd or even (zero is considered even).
+//     Use your function from Exercise 2 for this functionality.
+//     Output if the number is negative, positive, or zero
+// If the input is a string:
+//     If the string’s length is 1, output if the string (or char) is a vowel or not
+// Otherwise, output the length of the string
+// Design your program to include a switch and at least one if/else.
+// You must also make separate functions for all of the following program functionality:
+//     Determining if the input is a number, string, or otherwise. This function should be called analyzeInput
+//     Determining if a number is odd or even. Re-use your Exercise 2 function for this.
+//                    Determining if a number is <, >, or equal to zero
+//     Determining if the string is a single character or not
+//     Determining if the length 1 string is a vowel or not
+//     Add, commit, and push to GitHub.
+
+
+
+let input = prompt("Enter a string or a number");
+
+function analyzeInput(input) {
+    if (!isNaN(parseFloat(input))) {
+        console.log( input + " is a number");
+    }else if (isNaN(parseFloat(input))) {
+        console.log(input + " is a string");
+    }else {
+        console.log("Neither a string or a number");
+    }
+
 }
 
-console.log(analyzeColor1("yellow"));
+analyzeInput(input);
+
+function evenOrOdd(input) {
+    if (isNaN(parseFloat(input))) {
+        console.log("Strings aren't even or odd");
+    }
+    else if (parseFloat(input) % 2 === 0) {
+        console.log(input + " is an even number");
+    } else if (parseFloat(input) !== 0) {
+        console.log(input + " is an odd number");
+    } else {
+}}
+
+evenOrOdd(input);
+
+// console.log(isNaN())
+
