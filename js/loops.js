6ee72ceaca27d6025a27b8fb79098fb8c6674f1f
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-16 14:57:52 -0500

    Adding loops.js

---
+++ js/loops.js
@@ -0,0 +1,17 @@
+// var i = 0
+//
+// while (i < 100) {
+//     console.log("loop iteration " + i);
+//     i++;
+// }
+
+// var i = 10
+//
+// do {
+//     console.log("loop iteration " + i);
+//     i++
+// } while (i < 10);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("UwU loop iteration " + i);
+// }
