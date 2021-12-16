9c71e4bdeafb93490248b67fd81962fcd246eebd
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-16 15:54:24 -0500

    adding for_loops.js and loops.html

---
+++ js/for_loops.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// function showMultiplicationTable(num) {
+//     for (let i = 0; i < 11; i * num, i++) {
+//         console.log(num + " times " + i + " is " + (num * i));
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+
+// function tenRandomNumbers() {
+//     for (let i = 0; i < 10; i++) {
+//        let x = (Math.floor(Math.random(20) * 200));
+//         if (x % 2 !== 0) {
+//             console.log(x + " is odd");
+//         } else  {
+//             console.log(x + " is even");
+//         }
+//     }
+// }
+//
+// tenRandomNumbers();
+
+// for (let i = 100; i > 0; i -= 5) {
+//     console.log(i)
+// }
+
+for let i = 0; i < 10; i++) {
+    console.log()
+}
