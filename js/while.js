238c3374971b7ebd252679ba34fe67af7fe00192
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-22 14:37:15 -0500

    updating while.js

--- js/while.js
+++ js/while.js
@@ -1,9 +1,26 @@
 "use strict";
 
-let i = 1
+// let i = 1
 
-while (i < 65536) {
-    console.log(i * 2);
-    i *= 2
+// while (i < 65536) {
+//     console.log(i * 2);
+//     i *= 2
 
-}
+// }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    
+    let conesSold = Math.floor(Math.random() * 5) + 1;
+    if(conesSold > allCones) {
+        console.log("We can't do that, king. Don't got " + conesSold + " cones left");
+        continue;
+    }
+    allCones -= conesSold;
+    
+    console.log("We just sold " + conesSold + " cones!");
+    console.log("only " + allCones + " left to go!");    
+    
+
+} while (allCones > 0);
