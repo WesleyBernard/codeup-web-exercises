156d063fe033e39dae361631af85cb1f641d9ed7
Wesley Bernard <wesley.bernard64@gmail.com>
2021-12-22 14:16:34 -0500

    adding while.js and updating prime.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,3 +1,27 @@
 "use strict";
 
+// for (var i = 0; i < 100; i++) {
+//     console.log(i);
+//     if(i === 27) {
+//         console.log("We're hoppin off this ride, king");
+//         break;
+//     }   
+// }
+
+//these below codes I wrote while helpin someone study so ignore them, I may take them out.
+//****************************************************************************************** */
+
+// function not(input) {
+//     return !input
+// }
+
+// console.log(not(false));
+
+// function addOne(x) {
+//     return parseFloat(x) + 1;
+// }
+
+// console.log(addOne(Infinity));
+
+//********************************************************************************************* */
 
