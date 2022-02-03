e58702584be69bfb7617b112a2ac3600534bb5e2
Wesley Bernard <wesley.bernard64@gmail.com>
2022-02-03 12:33:17 -0500

    Updating jquery-first-exercise.js

--- js/jquery-first-exercise.js
+++ js/jquery-first-exercise.js
@@ -1,5 +1,7 @@
-"use strict"
+(function () {
+    "use strict"
+    $(document).ready(function () {
+        alert("Ding! DOM is done")
+    })
+})();
 
-$(document).ready(function () {
-    alert("Ding! DOM is done")
-})
