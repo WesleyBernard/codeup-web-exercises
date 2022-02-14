6b47b9c3712a6d8a6da704f0aa20ae5ef37f8a68
Wesley Bernard <wesley.bernard64@gmail.com>
2022-02-14 13:43:18 -0500

    Auto formatting

--- js/Weather-main.js
+++ js/Weather-main.js
@@ -11,6 +11,7 @@
             .setHTML(blerb)
         marker.setPopup(restaurantPopup);
     }
+
     function appendify(target, arr, index) {
         if (index === 0) {
             target.html("")
@@ -18,21 +19,25 @@
         let unix_timestamp = arr.dt;
         let date = new Date(unix_timestamp * 1000);
         date = new Date(unix_timestamp * 1000).toLocaleDateString();
-        target.append(`<div class="card" style="width: 25rem;">
-  <div class="card-body">
-    <h5 class="card-title">Date: ${date}</h5>
-    <h6 class="card-subtitle mb-2">Daytime feels like: ${arr.feels_like.day}</h6>
-    <h6 class="card-subtitle mb-2">Nighttime feels like: ${arr.feels_like.night}</h6>
-    <div class="card-text">Daytime tempature: ${arr.temp.day}</div>
-    <div class="card-text">Nighttime tempature: ${arr.temp.night}</div>
-    <div class="card-text">Low: ${arr.temp.min}</div>
-    <div class="card-text">High: ${arr.temp.max}</div>
-    <div class="card-text">Wind speed: ${arr.wind_speed}</div>
-    <div class="card-footer"><img src="http://openweathermap.org/img/wn/${arr.weather[0].icon}.png"</div>
-    <a href="https://en.wikipedia.org/wiki/Comic_Sans" class="card-link">comic sans wiki</a>
-  </div>
-</div>`)
+        //language=HTML
+        target.append(`
+            <div class="card" style="width: 25rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Date: ${date}</h5>
+                    <h6 class="card-subtitle mb-2">Daytime feels like: ${arr.feels_like.day}</h6>
+                    <h6 class="card-subtitle mb-2">Nighttime feels like: ${arr.feels_like.night}</h6>
+                    <div class="card-text">Daytime tempature: ${arr.temp.day}</div>
+                    <div class="card-text">Nighttime tempature: ${arr.temp.night}</div>
+                    <div class="card-text">Low: ${arr.temp.min}</div>
+                    <div class="card-text">High: ${arr.temp.max}</div>
+                    <div class="card-text">Wind speed: ${arr.wind_speed}</div>
+                    <div class="card-footer"><img src="http://openweathermap.org/img/wn/${arr.weather[0].icon}.png"
+                    </div>
+                    <a href="https://en.wikipedia.org/wiki/Comic_Sans" class="card-link">comic sans wiki</a>
+                </div>
+            </div>`)
     }
+
     function getify(lat, long, key) {
         $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely&appid=${key}&units=imperial`).done(function (results) {
             console.log(results)
@@ -45,6 +50,7 @@
             })
         })
     }
+
     mapboxgl.accessToken = WES_MAPBOX_API_KEY;
     var map = new mapboxgl.Map({
         container: 'map',
