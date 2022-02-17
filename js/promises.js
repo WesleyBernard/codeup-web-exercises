8b2010331b7ca7ee88cf0cd00e351febfb0153ca
Wesley Bernard <wesley.bernard64@gmail.com>
2022-02-17 17:53:42 -0500

    updating promise js and html with comments about the second exercise

--- js/promises.js
+++ js/promises.js
@@ -6,4 +6,19 @@
         .catch(e => console.log(e))
 }
 seeCommits("WesleyBernard", "codeup-web-exercises");
+
+    // function wait(MS) {
+    //     let myPromise = new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             if (Math.random() > 0.5) {
+    //                 resolve();
+    //             } else {
+    //                 reject();
+    //             }
+    //         }, MS)
+    //     })
+    // }
+
+    // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+    // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 })();
